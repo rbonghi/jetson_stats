13ae73a808874c23ba5150bc2503ef9e08a6821b
Raffaello Bonghi <raffaello@rnext.it>
2019-05-19 11:01:42 +0200

    Improve setup config

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ if os.getuid() != 0:
 with open("README.md", "r") as fh:
     long_description = fh.read()
     
+project_homepage = "https://github.com/rbonghi/jetson_stats"
+    
 def install_packages():
     return [('/opt/jetson_stats', ['scripts/jetson_variables', 
                                    'scripts/jetson-performance.sh']),
@@ -57,16 +59,28 @@ setuptools.setup(
     version="1.6.0",
     author="Raffaello Bonghi",
     author_email="raffaello@rnext.it",
-    description="Interactive system-monitor process viewer for nvidia Jetson Nano, AGX Xavier, TX2, TX1",
+    description="Interactive system-monitor process viewer for NVIDIA Jetson Nano, AGX Xavier, TX2, TX1",
     license='LICENSE',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rbonghi/jetson_stats",
-    packages=setuptools.find_packages(),
+    url=project_homepage,
+    download_url=(project_homepage + "/archive/master.zip"),
+    project_urls={
+        "How To": (project_homepage + "/tree/master/docs"),
+        "Examples": (project_homepage + "/tree/master/examples"),
+        "Bug Reports": (project_homepage + "/issues"),
+        "Source": (project_homepage + "/tree/master")
+    },
+    packages=setuptools.find_packages(exclude=['examples', 'scripts']), # Required
+    keywords=(
+        "jetson_stats jtop python system-monitor docker \
+         nvidia Jetson Nano Xavier TX2 TX1 process viewer"
+    ),
     classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux", 
+        # Development status
+        'Development Status :: 4 - Beta',
+        # Audiencence and topics
+        'Intended Audience :: Developers',
         "Topic :: Software Development :: Embedded Systems",
         "Topic :: Software Development :: Debuggers", 
         "Topic :: Software Development :: Libraries",
@@ -79,7 +93,25 @@ setuptools.setup(
         "Topic :: System :: Shells",
         "Topic :: System :: Systems Administration",
         "Topic :: Terminals",
+        # License
+        "License :: OSI Approved :: MIT License",
+        # Programming and Operative system
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: POSIX :: Linux", 
         ],
+    # Requisites
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    platforms=["linux", "linux2", "darwin"],
+    # Zip safe configuration
+    # https://setuptools.readthedocs.io/en/latest/setuptools.html#setting-the-zip-safe-flag
+    zip_safe=False,
     # Add jetson_variables in /opt/jetson_stats
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     data_files=install_packages(),
