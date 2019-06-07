79ae5ecf4705acd3cbdf41f16eaddba4dfb8d032
Raffaello <raffaello@rnext.it>
2019-06-07 14:53:52 +0200

    Fix license

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     author="Raffaello Bonghi",
     author_email="raffaello@rnext.it",
     description="Interactive system-monitor and process viewer for all NVIDIA Jetson [Nano, AGX Xavier, TX1, TX2]",
-    license='LICENSE',
+    license='MIT',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=project_homepage,
