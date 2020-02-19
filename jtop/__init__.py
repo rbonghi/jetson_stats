9d90f31b5a098d741f3bfbae00957c64d6645af2
Raffaello Bonghi <raffaello@rnext.it>
2020-02-19 18:10:39 +0000

    Fix rc version 2

--- jtop/__init__.py
+++ jtop/__init__.py
@@ -27,5 +27,5 @@ from .core import JetsonClocks
 from .core import import_os_variables, StatusObserver
 
 __author__ = "Raffaello Bonghi <raffaello@rnext.it>"
-__version__ = "1.8.4.rc1"
+__version__ = "1.8.4.rc2"
 # EOF
