d1a2118cd5742c8b44b471e3000aa8f97a9a6f9e
Raffaello Bonghi <raffaello@rnext.it>
2021-03-01 15:40:49 +0000

    Fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Raffaello Bonghi'
 author = 'Raffaello Bonghi'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.3'
+release = '3.1.0'
 
 # -- General configuration ---------------------------------------------------
 
