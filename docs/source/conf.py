2c040ffa2a727ab152908a1b3fecee2f8ee3a7df
Raffaello Bonghi <raffaello@rnext.it>
2022-05-20 15:39:00 +0100

    Fix #212

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020-2021, Raffaello Bonghi'
 author = 'Raffaello Bonghi'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.4'
+release = '4.0.0'
 
 # -- General configuration ---------------------------------------------------
 
