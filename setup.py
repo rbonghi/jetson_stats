18871b62ab5ef53cc232b8194fa1a8dc4bac7be5
Raffaello Bonghi <raffaello@rnext.it>
2023-01-04 12:29:03 +0100

    Add other popular python versions

--- setup.py
+++ setup.py
@@ -219,11 +219,11 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: POSIX :: Linux"],
     # Requisites
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
