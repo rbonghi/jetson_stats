a84432349219267b9c74435a72a4a872d031c608
Raffaello Bonghi <raffaello@rnext.it>
2023-01-07 16:58:11 +0100

    Fix module load

--- setup.py
+++ setup.py
@@ -229,6 +229,7 @@ setup(
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     platforms=["linux", "linux2", "darwin"],
+    install_requires=['distro'],
     # Zip safe configuration
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#setting-the-zip-safe-flag
     zip_safe=False,
