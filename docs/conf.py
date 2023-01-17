628d4ea9aa9314c51b36259b696196102cfdf65b
Raffaello Bonghi <raffaello@rnext.it>
2023-01-17 20:29:59 +0000

    minor fixes

--- docs/conf.py
+++ docs/conf.py
@@ -68,6 +68,7 @@ copybutton_selector = "div:not(.no-copybutton) > div.highlight > pre"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+# Reference: https://pradyunsg.me/furo/
 html_theme = "furo"
 html_favicon = "images/favicon.png"
 
