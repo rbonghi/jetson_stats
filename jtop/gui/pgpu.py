76a653f974fe418a0f481cd14929f1e0d7fe2a53
Raffaello Bonghi <raffaello@rnext.it>
2023-03-05 13:40:31 +0000

    Minor fix all page

--- jtop/gui/pgpu.py
+++ jtop/gui/pgpu.py
@@ -55,7 +55,7 @@ def compact_gpu(stdscr, pos_y, pos_x, width, jetson):
             'color': NColors.green() | curses.A_BOLD,
             'online': False,
             'coffline': NColors.igreen(),
-            'message': 'NVIDIA GPU NOT AVAILABLE',
+            'message': 'NVIDIA GPU NOT DETECTED/AVAILABLE',
         }
         basic_gauge(stdscr, pos_y, pos_x, width - 2, data)
         line_counter = 1
